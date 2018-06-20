ccea92b40ee7c7b2ad57ab6fe5b86785a7177d4b
SweetAlice <1374909436@qq.com>
2018-06-20 16:18:52 +0800

    提交左边栏

--- src/components/leftmenu.js
+++ src/components/leftmenu.js
@@ -1,10 +1,10 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
 //let yeomanImage = require('../images/yeoman.png');
-let menuData = require('json!../data/leftMenu.json');
+let menuData = require('../data/leftMenu.json');
 //import data from  '../data/leftMenu2.js';
 //let imgData = require('../data/imgData.json');
-console.log(menuData)
+//console.log(menuData)
 
 import React from 'react';
 import { Menu, Icon } from 'antd';
@@ -12,35 +12,127 @@ const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
 class LeftComponent extends React.Component {
-  handleClick = (e) => {
-    console.log('click ', e);
-  }
-  render() {
-  	let leftMenu = {width: 256,height:'calc(100% -  47px)',position:'absolute'};
-    return (
-      { 
-      	<Menu
-        onClick={this.handleClick}
-        style={leftMenu}
-        defaultSelectedKeys={['0']}
-        defaultOpenKeys={['sub0']}
-        mode="inline"
-        >
-			    return { menuData.map(function(item, index){
-				      	    <SubMenu key="sub{index}" title={<span><Icon type="{item.type}" /><span>{{item.menuTitle}}</span></span>}>
-				            {item.subMenu.map(function(item2,index2){
-				            	    <MenuItemGroup key="g{index2}" title="{item2.label}">
-					            	    {item2.subMenu.map(function(item3,index3){
-					            	    	  <Menu.Item key="index3">{{item3.label}}</Menu.Item>
-					            	    })}
-						               </MenuItemGroup>
-				            })} 
-				            </SubMenu>
-						})}
-	      </Menu>
-      }
-    );
-  }
+	  //递归函数生成左侧菜单树
+    //这里通过函数的形式，通过递归自身function的方式来生成菜单树的子菜单
+    formSubmenusChild(obj,sign){
+        let cHtml=<div></div>,childArray=obj.subMenu
+        if("undefined"!=typeof(childArray)&&childArray.length>0) {
+          cHtml = childArray.map((item, i) => {
+          	      let  underChild =item.subMenu,signTip=""
+			          	if("undefined"!=typeof(underChild)&&underChild.length>0) {
+					        	signTip = false;
+					        }else{
+					        	signTip = true;
+					        }
+			          	return this.formSubmenusChild(item,signTip);
+            });
+            if(sign){
+            	//title={<span><Icon type="mail" /><span>Navigation One</span></span>}
+            	//title={obj.label}><Icon type={obj.type}/> {cHtml}</SubMenu>
+			           	return <SubMenu key={obj.linkurl} title={obj.label} >{cHtml}</SubMenu>
+			      }else{
+			           return  <MenuItemGroup key={obj.linkurl} title={obj.label}></MenuItemGroup>	
+			      }
+           
+        }else{
+            return <Menu.Item  key={obj.linkurl}>{obj.label}</Menu.Item>
+            //routeurl={obj.routerurl}
+        }
+    }
+    handleClick(e) {
+        console.log('click ', e);
+//      const {actions}=this.props;
+//
+//      let menuInfo={
+//          //打印antd Menu组件的onClick返回值（e）可以看出通过e.item.props来获取我们item组件的自定义属性
+//          routeurl:e.routeurl,
+//          url: e.key,
+//          target:'mainFrame'
+//      }
+//      //根据route路由地址和iframe地址的值判断使用哪个作为菜单页面展示方式
+//      actions.onLoadMain(menuInfo);
+    }
+    render() {
+//      const {columnMenuInfo,backEndMenuUrl} = this.props;
+//      let columnMenus=columnMenuInfo.toJSON();
+        let leftMenu = {width: 256,height:'calc(100% -  47px)',position:'absolute'};
+        let html=menuData.map((obj,i)=> {
+        	    let childArray = obj.subMenu;
+            if ("undefined"!=typeof(childArray)&&childArray.length>0) {
+                return this.formSubmenusChild(obj,true);
+            }else{
+                //这里的routeurl是路由地址，是自定义的一个属性
+                return <Menu.Item  key={obj.linkurl} title={obj.label}>{obj.label}</Menu.Item>
+                //routeurl={obj.routerurl}
+            }
+        });
+
+        return (
+            <Menu theme="dark"
+                onClick={this.handleClick}
+                  style={leftMenu}
+                  defaultOpenKeys={['Nav2']}
+                  selectedKeys={["g3sub1"]}
+                  mode="inline"
+            >
+                {html}
+            </Menu>
+        )
+    }
 }
+
+//function mapStateToProps(state) {
+//  const {middleTheme} = state;
+//  return {
+//      backEndMenuUrl: middleTheme.get('backEndMenuUrl'),
+//      columnMenuInfo:middleTheme.get('columnMenuInfo')
+//  }
+//}
+//
+//function mapDispatchToProps(dispatch) {
+//  return {
+//      actions: bindActionCreators(themeActions, dispatch)
+//  };
+//}
+
+	
+	
+	
+//	
+
+//render() {
+//	let leftMenu = {width: 256,height:'calc(100% -  47px)',position:'absolute'};
+//  return (
+//    
+//    	<Menu
+//      onClick={this.handleClick}
+//      style={leftMenu}
+//      defaultSelectedKeys={['0']}
+//      defaultOpenKeys={['sub0']}
+//      mode="inline"
+//      >
+//    	 <Menu.Item key="mail">
+//        <Icon type="mail" />Navigation One
+//      </Menu.Item>
+//      
+//			      {menuData.map(function(item,index){
+//			      	console.log(item,index)
+//			      	
+//				      	   <SubMenu key={index} title={<span><Icon type={item.type} /><span>{{item.menuTitle}}</span></span>}>
+//					            {item.subMenu.map(function(item2,index2){
+//					            	console.log(item2,index2)
+//					            	    <MenuItemGroup key={index2.link} title={item2.label}>
+//						            	    {item2.subMenu.map(function(item3,index3){
+//						            	    	  <Menu.Item key={index3.link}>{{item3.label}}</Menu.Item>
+//						            	    })}
+//							               </MenuItemGroup>
+//					            })} 
+//				            </SubMenu>
+//						})}
+//			      
+//			  </Menu>
+//  );
+// }
+//}
 
 export default LeftComponent;
--- src/router.js
+++ src/router.js
@@ -1,34 +1,15 @@
 import React from 'react';
 import { Router, Route, Switch, IndexRoute } from 'react-router';
 import IndexPage from './routes/IndexPage';
-import Products from './routes/Products';
-import LoginPage from './routes/Login';
-//import Main from './routes/Main';
-//import TablePage from './routes/TablePage';
-//import AcePage from './routes/ReactAce';
-//import ChartsPage from './routes/Charts';
-//import InputPage from './routes/Input';
-//import ModalPage from './routes/Modal';
-//import MessagePage from './routes/Message';
-//import Maps from './routes/Maps';
-//import TextEdtor from './routes/TextEdtor';
+import pageA from './components/content/componentsA';
+import pageB from './components/content/componentsB';
 
 function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={LoginPage} />
-        <Route path="/products" exact component={Products} />
-//      <Route path="/login" exact component={LoginPage} />
-//      <Route path="/main" exact component={Main} />
-//      <Route path="/tables" exact component={TablePage} />
-//      <Route path="/ace" exact component={AcePage} />
-//      <Route path="/charts" exact component={ChartsPage} />
-//      <Route path="/inputs" exact component={InputPage} />
-//      <Route path="/coms/modal" exact component={ModalPage} />
-//      <Route path="/coms/message" exact component={MessagePage} />
-//      <Route path="/coms/maps" exact component={Maps} />
-//      <Route path="/coms/textEdtor" exact component={TextEdtor} />
+        <Route path="/" exact component={pageA} />
+        <Route path="/pageB" exact component={pageB} />
       </Switch>
     </Router>
   );
