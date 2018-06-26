302b5ee382a0b5e9fa137006faf0c61d7936ee6f
SweetAlice <1374909436@qq.com>
2018-06-26 17:51:51 +0800

    星期二提交

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,49 @@
 
 import React from 'react';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+const { Header, Content, Sider } = Layout;
 
-import Header from './header';
+
+//import HeaderContent from './header';
 import Leftmenu from './leftmenu';
 
 //let yeomanImage = require('../images/yeoman.png');
 //let imgData = require('../data/imgData.json');
-
+////	      <HeaderContent/>
 class AppComponent extends React.Component {
+	
   render() {
+  	let leftStyle= {minHeight:'calc(100% -  64px)',position:"absolute"},
+        logoStyle ={width: "200px",height: "64px",background:"url(../images/16.jpg) no-repeat",backgroundSize:"100% 100%",float:" left"},
+        divStyle ={width:"calc(50% - 100px)",height:"64px",float:"left",textAlign:"right",padding:'0 50px'},
+        divoutStyle ={width:"calc(50% - 100px)",height:"64px",float:"left",textAlign:"right",padding:"0 50px"}
     return (
-      <div className="index">
-	       <Header/>
-	       <Leftmenu/>
-	       
-      </div>
+	  <Layout>
+	      <Header className="header" style={{padding:0,width:'100%',height:"64px",background:"black",clear:"both"}}>
+		      <div className="logo" style={logoStyle}/>
+		      <div style={{padding:0,margin:0,color:"white",width:"calc(100% - 200px)",height: "64px",lineHeight: "64px",float: "left"}}>
+		        <div style={divStyle}>header</div>
+		        <div style={divoutStyle}>退出</div>
+		      </div>
+	      </Header>
+		    <Layout>
+	           <Sider width={200} style={leftStyle}>
+	               <Leftmenu/>
+	           </Sider>
+	        <Layout style={{ padding: '0 24px 24px',marginLeft: "200px",height: 'calc(100% - 130px)',position: 'absolute',width: 'calc(100% - 300px)'}}>
+		        <Breadcrumb style={{ margin: '16px 0' }}>
+		          <Breadcrumb.Item>Home</Breadcrumb.Item>
+		          <Breadcrumb.Item>List</Breadcrumb.Item>
+		          <Breadcrumb.Item>App</Breadcrumb.Item>
+		        </Breadcrumb>
+		        <Content style={{ background: '#fff', padding: 24, margin: 0, minHeight:'calc(100% - 64px)',width: '100%' }}>
+		          Content
+		        </Content>   
+		      </Layout>
+        </Layout>
+  	</Layout>
     );
+  
   }
 }
 
--- src/components/leftmenu.js
+++ src/components/leftmenu.js
@@ -5,7 +5,6 @@ let menuData = require('../data/leftMenu.json');
 //import data from  '../data/leftMenu2.js';
 //let imgData = require('../data/imgData.json');
 //console.log(menuData)
-
 import React from 'react';
 import { Menu, Icon } from 'antd';
 const SubMenu = Menu.SubMenu;
@@ -15,7 +14,8 @@ class LeftComponent extends React.Component {
 	  //递归函数生成左侧菜单树
     //这里通过函数的形式，通过递归自身function的方式来生成菜单树的子菜单
     formSubmenusChild(obj,sign){
-        let cHtml=<div></div>,childArray=obj.subMenu
+        let cHtml=<div></div>,childArray=obj.subMenu;
+//      console.log(obj)
         if("undefined"!=typeof(childArray)&&childArray.length>0) {
           cHtml = childArray.map((item, i) => {
           	      let  underChild =item.subMenu,signTip=""
@@ -27,11 +27,10 @@ class LeftComponent extends React.Component {
 			          	return this.formSubmenusChild(item,signTip);
             });
             if(sign){
-            	//title={<span><Icon type="mail" /><span>Navigation One</span></span>}
-            	//title={obj.label}><Icon type={obj.type}/> {cHtml}</SubMenu>
-			           	return <SubMenu key={obj.linkurl} title={obj.label} >{cHtml}</SubMenu>
+            	//title={<span><Icon type="mail" /><span>Navigation One</span></span>}//增加图标
+			           return <SubMenu key={obj.linkurl} title={<span><Icon type={obj.type}/>{obj.label}</span>}>{cHtml}</SubMenu>
 			      }else{
-			           return  <MenuItemGroup key={obj.linkurl} title={obj.label}></MenuItemGroup>	
+			           return  <MenuItemGroup key={obj.linkurl} title={obj.label} ></MenuItemGroup>	
 			      }
            
         }else{
@@ -39,23 +38,14 @@ class LeftComponent extends React.Component {
             //routeurl={obj.routerurl}
         }
     }
+   
     handleClick(e) {
         console.log('click ', e);
-//      const {actions}=this.props;
-//
-//      let menuInfo={
-//          //打印antd Menu组件的onClick返回值（e）可以看出通过e.item.props来获取我们item组件的自定义属性
-//          routeurl:e.routeurl,
-//          url: e.key,
-//          target:'mainFrame'
-//      }
-//      //根据route路由地址和iframe地址的值判断使用哪个作为菜单页面展示方式
-//      actions.onLoadMain(menuInfo);
     }
     render() {
 //      const {columnMenuInfo,backEndMenuUrl} = this.props;
-//      let columnMenus=columnMenuInfo.toJSON();
-        let leftMenu = {width: 256,height:'calc(100% -  47px)',position:'absolute'};
+//      let columnMenus=columnMenuInfo.toJSON();width: 256,height:'calc(100% -  47px)'position:'absolute'
+        let leftMenu = {height:'calc(100% -  47px)',position:'absolute',background:"black"}; 
         let html=menuData.map((obj,i)=> {
         	    let childArray = obj.subMenu;
             if ("undefined"!=typeof(childArray)&&childArray.length>0) {
@@ -66,73 +56,31 @@ class LeftComponent extends React.Component {
                 //routeurl={obj.routerurl}
             }
         });
-
+//      console.log(html)
         return (
-            <Menu theme="dark"
-                onClick={this.handleClick}
-                  style={leftMenu}
+            <Menu theme="dark" style={leftMenu}
+                  onClick={this.handleClick}
+                  defaultSelectedKeys={['g4']}
+                  selectedKeys={['g4']}
                   defaultOpenKeys={['Nav2']}
-                  selectedKeys={["g3sub1"]}
-                  mode="inline"
-            >
+                  mode="inline">
                 {html}
             </Menu>
         )
     }
 }
 
-//function mapStateToProps(state) {
-//  const {middleTheme} = state;
-//  return {
-//      backEndMenuUrl: middleTheme.get('backEndMenuUrl'),
-//      columnMenuInfo:middleTheme.get('columnMenuInfo')
-//  }
-//}
-//
-//function mapDispatchToProps(dispatch) {
-//  return {
-//      actions: bindActionCreators(themeActions, dispatch)
-//  };
-//}
-
-	
-	
-	
-//	
-
-//render() {
-//	let leftMenu = {width: 256,height:'calc(100% -  47px)',position:'absolute'};
-//  return (
-//    
-//    	<Menu
-//      onClick={this.handleClick}
-//      style={leftMenu}
-//      defaultSelectedKeys={['0']}
-//      defaultOpenKeys={['sub0']}
-//      mode="inline"
-//      >
-//    	 <Menu.Item key="mail">
-//        <Icon type="mail" />Navigation One
-//      </Menu.Item>
-//      
-//			      {menuData.map(function(item,index){
-//			      	console.log(item,index)
-//			      	
-//				      	   <SubMenu key={index} title={<span><Icon type={item.type} /><span>{{item.menuTitle}}</span></span>}>
-//					            {item.subMenu.map(function(item2,index2){
-//					            	console.log(item2,index2)
-//					            	    <MenuItemGroup key={index2.link} title={item2.label}>
-//						            	    {item2.subMenu.map(function(item3,index3){
-//						            	    	  <Menu.Item key={index3.link}>{{item3.label}}</Menu.Item>
-//						            	    })}
-//							               </MenuItemGroup>
-//					            })} 
-//				            </SubMenu>
-//						})}
-//			      
-//			  </Menu>
-//  );
-// }
-//}
 
 export default LeftComponent;
+// state = {
+//	    collapsed: false,
+//	  }
+//	  toggleCollapsed = () => {
+//	    this.setState({
+//	      collapsed: !this.state.collapsed,
+//	    });
+//	  }
+// inlineCollapsed={this.state.collapsed}
+////            <Button type="primary" onClick={this.toggleCollapsed} style={{ marginBottom: 16 }}>
+//			          <Icon type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'} />
+//			        </Button>
